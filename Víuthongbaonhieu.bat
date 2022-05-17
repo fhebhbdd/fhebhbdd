ee4a77cdc90da92068a042829fb69f63ab38b6f8
fhebhbdd <103022873+fhebhbdd@users.noreply.github.com>
2022-05-17 12:33:45 +0700

    Create Víuthongbaonhieu.bat

---
+++ Víuthongbaonhieu.bat
@@ -0,0 +1,7 @@
+x=msgbox ("error" ,2+16, "Virus")
+X=msgbox(“chao”,2+4,”error”)
+X=msgbox(“may ban bi virus ”,2+14,”error”)
+X=msgbox(“dang quet”,12+4,”dang quet”)
+X=msgbox(“error”,12+4,”end”)
+X=msgbox(“error”,3+5,”microsoft windows”)
+
